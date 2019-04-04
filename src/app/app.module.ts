4d4979b87de9ed15fce0816c4ff2dda18ff6c2b0
Daniel Vicario <danigv.email@gmail.com>
2019-04-04 13:19:21 +0200

    added examples

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Routes, RouterModule } from '@angular/router';
+import { MyHomeComponent } from './my-home-component/my-home-component.component';
+import { MyAboutComponent } from './my-about-component/my-about-component.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home',  component: MyHomeComponent },
+  { path: 'products/:productID',  component: MyHomeComponent },
+  { path: 'about', component: MyAboutComponent }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyHomeComponent,
+    MyAboutComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/my-about-component/my-about-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-about-component',
+  templateUrl: './my-about-component.component.html',
+  styleUrls: ['./my-about-component.component.scss']
+})
+export class MyAboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
